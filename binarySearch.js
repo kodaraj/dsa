548128444dd3f1745a4b979f0f022120524fe944
kodaraj <aamirjamal619@gmail.com>
2024-12-14 18:15:11 +0530

    added new solution

---
+++ binarySearch.js
@@ -0,0 +1,37 @@
+// https://leetcode.com/problems/binary-search/description/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function (nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] == target) {
+      return mid;
+    }
+
+    if (target < nums[mid]) {
+      right = mid - 1;
+    } else if (target > nums[mid]) {
+      left = mid + 1;
+    }
+  }
+  return -1;
+};
+
+console.log(search([-1, 0, 3, 5, 9, 12], 9)); // 4
+console.log(search([-1, 0, 3, 5, 9, 12], 2)); // -1
+console.log(search([5], 5)); // 0
+
+/*
+
+Method 1 - Binary Seach Algorithm
+- Time complexity = O(log n)
+- Space complexity = 
+
+*/
